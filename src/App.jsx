da1dd4dc89269300141289fb7ff6293987f59a13
Oscar <orivas155@gmail.com>
2024-09-25 16:08:55 -0600

    feat: tic tac toe with few challenges completed

---
+++ src/App.jsx
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+
+import './App.css'
+
+function Square({ value, onSquareClick }) {
+  return <button className="square"
+                 onClick={onSquareClick}>
+                 {value}
+         </button>;
+}
+
+function Board({xIsNext, squares, onPlay}) {
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(index) {
+    if (squares[index] || calculateWinner(squares)) {
+      return
+    }
+    const nextSquares = squares.slice()
+    if(xIsNext) {
+      nextSquares[index] = "X"
+    } else {
+      nextSquares[index] = "O"
+    }
+    onPlay(nextSquares)
+  }
+  return <>
+     <div className="status">{status}</div>
+      {[0, 3, 6].map((pivot) => {
+        return(
+          <div key={pivot} className="board-row">
+            {squares.slice(pivot, pivot + 3).map((square, index) => {
+               const squareIndex = pivot + index
+               return(
+                 <Square key={squareIndex}
+                         value={square}
+                         onSquareClick={() => handleClick((squareIndex))}/>
+               )
+            })}
+          </div>)
+      })}
+   </>
+}
+
+function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0)
+  const [reverse, setToReverse] = useState(false)
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove]
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove)
+  }
+
+  function handleReverseClick() {
+    setToReverse(!reverse)
+  }
+
+  const moves = history.map((squares, move) =>{
+    let description;
+    if(move) {
+      description = `Go to move # ${move}`
+    } else {
+      description = 'Go to game start'
+    }
+  
+    if (move == currentMove) {
+      return(
+        <li key={move}>
+          {description}
+        </li>
+      )
+    }
+
+    return(
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <div className="game-info">
+          <button onClick={handleReverseClick}>Reverse</button>
+        </div>
+        <ol>{reverse ? moves : moves.toReversed()}</ol>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return <Game/>
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], // positions where there are a winner
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default App
